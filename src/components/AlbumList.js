52098fd12ee60562c562a61e9eb8dc2a4335b223
Baskara Reddy <baskara.ar@gmail.com>
2018-11-08 09:51:32 -0800

    Initial simple app to display a text message and pull the JSON content from a remote endpoint and display onscreen

---
+++ src/components/AlbumList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+//const AlbumList = () => {
+
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentWillMount() {
+        //console.log('componentWillMount in AlbumList');
+        //debugger;
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }));
+    }
+    
+    
+renderAlbums() {
+return this.state.albums.map(album => 
+     <AlbumDetail key={album.title}>{album.title}</AlbumDetail>
+   );
+}
+
+    render() {
+        console.log(this.state);
+    return (
+        <View>
+              {this.renderAlbums()}
+        </View>
+
+    );
+  }
+}
+
+export default AlbumList;
